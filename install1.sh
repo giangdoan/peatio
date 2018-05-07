59309ba2a31b040a42b095cae6aa4787ac8139c1
Giang Doan <gianghdoan@gmail.com>
2018-05-07 06:24:58 -0700

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -43,11 +43,16 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CA
 sudo apt-get install -y apt-transport-https ca-certificates
 
 # Add our APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
+# sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+# sudo apt-get update
 
 # Install Passenger + Nginx
-sudo apt-get install -y --allow-unauthenticated nginx-extras passenger
+# sudo apt-get install -y --allow-unauthenticated nginx-extras passenger
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+sudo apt-get install -y nginx-extras
 #bitcoind
 sudo cp passenger.conf /etc/nginx/passenger.conf
 #sudo echo "include  /etc/nginx/passenger.conf;" >> /etc/nginx/nginx.conf
